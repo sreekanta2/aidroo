656f6fe0ace56b243d2f3a4bbd7cc55b153d9e26
sreekanta2 <sreekanta03s@gmail.com>
2024-06-20 23:55:26 +0600

    footer code fix

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -6,20 +6,23 @@ import Link from "next/link";
 import { useEffect, useState } from "react";
 
 export default function Footer() {
-  const [open, setOpen] = useState(window.innerWidth > 640);
-
-  const updateFooter = () => {
-    if (window.innerWidth > 640) {
-      setOpen(true);
-    } else {
-      setOpen(false);
-    }
-  };
+  const [open, setOpen] = useState(false);
 
   useEffect(() => {
-    window.addEventListener("resize", updateFooter);
-    // Set the initial state based on the window width
+    // Ensure this code only runs on the client
+    const updateFooter = () => {
+      if (typeof window !== "undefined") {
+        if (window.innerWidth > 640) {
+          setOpen(true);
+        } else {
+          setOpen(false);
+        }
+      }
+    };
+
     updateFooter();
+    window.addEventListener("resize", updateFooter);
+
     return () => {
       window.removeEventListener("resize", updateFooter);
     };
@@ -28,82 +31,78 @@ export default function Footer() {
   return (
     <div className="patenBackgroundImage h-fit pt-14 bg-[#f3fcff]">
       <div className="max-w-[1280px] mx-auto px-8">
-        {open ? (
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
-            {footerLinks.length > 0 &&
-              footerLinks.map((footerLink, index) => (
-                <div key={index} className="flex flex-col">
-                  <h1 className="text-xl font-bold pb-2">{footerLink.title}</h1>
-                  {footerLink.links.length > 0 && (
-                    <ul className="text-gray-600">
-                      {footerLink.links.map((link, linkIndex) => (
-                        <li key={linkIndex}>
-                          {link.title ? (
-                            <Link
-                              href={link.link}
-                              className="hover:text-blue-500 text-sm"
-                            >
-                              {link.title}
-                            </Link>
-                          ) : link.image ? (
-                            <Image
-                              src={link.image}
-                              width={150}
-                              height={75}
-                              alt={link.image}
-                              className="mt-4"
-                            />
-                          ) : null}
-                        </li>
-                      ))}
-                    </ul>
-                  )}
-                </div>
-              ))}
-          </div>
-        ) : (
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
-            {footerLinks.length > 0 &&
-              footerLinks.map((footerLink, index) => (
-                <details
-                  key={index}
-                  className="flex flex-col collapse collapse-arrow"
-                >
-                  <summary className="collapse-title text-xl font-medium">
-                    <h1 className="text-xl font-bold cursor-default ">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
+          {footerLinks.length > 0 &&
+            footerLinks.map((footerLink, index) => (
+              <div key={index} className="flex flex-col">
+                {open ? (
+                  <>
+                    <h1 className="text-xl font-bold pb-2">
                       {footerLink.title}
                     </h1>
-                  </summary>
-                  {footerLink.links.length > 0 && (
-                    <ul className="text-gray-600 collapse-content ms-2">
-                      {footerLink.links.map((link, linkIndex) => (
-                        <li key={linkIndex}>
-                          {link.title ? (
-                            <Link
-                              href={link.link}
-                              className="hover:text-blue-500 text-sm"
-                            >
-                              {link.title}
-                            </Link>
-                          ) : link.image ? (
-                            <Image
-                              src={link.image}
-                              width={150}
-                              height={75}
-                              alt={link.image}
-                              className="mt-4"
-                            />
-                          ) : null}
-                        </li>
-                      ))}
-                    </ul>
-                  )}
-                </details>
-              ))}
-          </div>
-        )}
+                    {footerLink.links.length > 0 && (
+                      <ul className="text-gray-600">
+                        {footerLink.links.map((link, linkIndex) => (
+                          <li key={linkIndex}>
+                            {link.title ? (
+                              <Link
+                                href={link.link}
+                                className="hover:text-blue-500 text-sm"
+                              >
+                                {link.title}
+                              </Link>
+                            ) : link.image ? (
+                              <Image
+                                src={link.image}
+                                width={150}
+                                height={75}
+                                alt={link.image}
+                                className="mt-4"
+                              />
+                            ) : null}
+                          </li>
+                        ))}
+                      </ul>
+                    )}
+                  </>
+                ) : (
+                  <details className="collapse collapse-arrow">
+                    <summary className="collapse-title text-xl font-medium">
+                      <h1 className="text-xl font-bold pb-2">
+                        {footerLink.title}
+                      </h1>
+                    </summary>
+                    {footerLink.links.length > 0 && (
+                      <ul className="text-gray-600 collapse-content">
+                        {footerLink.links.map((link, linkIndex) => (
+                          <li key={linkIndex}>
+                            {link.title ? (
+                              <Link
+                                href={link.link}
+                                className="hover:text-blue-500 text-sm"
+                              >
+                                {link.title}
+                              </Link>
+                            ) : link.image ? (
+                              <Image
+                                src={link.image}
+                                width={150}
+                                height={75}
+                                alt={link.image}
+                                className="mt-4"
+                              />
+                            ) : null}
+                          </li>
+                        ))}
+                      </ul>
+                    )}
+                  </details>
+                )}
+              </div>
+            ))}
+        </div>
         <div className="flex justify-between items-center h-24">
-          <Image src={logo} width={100} height={100} alt="Aidroo logo" />
+          <Image src={logo} width={100} height={100} alt="aidroo logo" />
           <p className="font-semibold">
             All rights reserved
             <span className="text-secondary-A100"> aidroo</span> 2024
